d5f6f52fffc963badb243712d341d5712d9a6783
Sean Osier <sean@seanosier.com>
2021-01-07 00:21:37 -0500

    Implement vector magnitude, dot_product, cosine similarities

--- 01-Linear_Algebra/linear_algebra.py
+++ 01-Linear_Algebra/linear_algebra.py
@@ -1,3 +1,5 @@
+from math import sqrt, acos, degrees
+
 def is_numeric(object):
     return type(object) == int or type(object) == float
 
@@ -16,6 +18,11 @@ class Vector():
         return str(self.vector)
 
     def __len__(self):
+        """
+        This is actually more like the "dimension" of the vector, but using the
+        Python `len` command will return the `len` the same way it would for a
+        list of numbers
+        """
         return len(self.vector)
 
     def __eq__(self, other):
@@ -56,3 +63,46 @@ class Vector():
     def __truediv__(self, other):
         assert(is_numeric(other))
         return Vector(*[1/other * num for num in self.vector])
+
+    def magnitude(self):
+        """
+        "Magnitude", "size", or "length" of the vector.
+
+        Calculated via the Pythagorean thereom. In that way, this is also the
+        distance from the origin to the end of the vector.
+        """
+        return sqrt(sum([num**2 for num in self.vector]))
+
+    def dot_product(self, other):
+        """
+        "Dot product" or "sum product" of two vectors
+        """
+        assert(isinstance(other, Vector))
+        assert(len(self) == len(other))
+        return sum([x * y for x, y in zip(self, other)])
+
+    def dot(self, other):
+        """
+        Short convenience function for accessing `.dot_product(...)` since this
+        is such a common vector operation
+        """
+        return self.dot_product(other)
+
+    def cosine_similarity(self, other):
+        return (
+            self.dot(other) /
+            (self.magnitude() * other.magnitude())
+        )
+
+    def cosine_distance(self, other):
+        """
+        Convert the cosine similarity between two vectors into a distance metric
+        """
+        return 1 - self.cosine_similarity(other)
+
+    def angle_between(self, other, radians=False):
+        angle = acos(self.cosine_similarity(other))
+        if not radians:
+            angle = degrees(angle)
+
+        return angle
--- 01-Linear_Algebra/linear_algebra_tests.py
+++ 01-Linear_Algebra/linear_algebra_tests.py
@@ -1,6 +1,9 @@
+from math import sqrt, pi, cos
 import traceback
 
 from linear_algebra import is_numeric, Vector
+
+PRECISION = 10
 
 def test_is_numeric():
     try:
@@ -43,6 +46,47 @@ def test_Vector():
         assert((3 * v) == Vector(3, 6, 6))
         assert((v * 3) == Vector(3, 6, 6))
         assert((v / 3) == Vector(1/3, 2/3, 2/3))
+
+        # Vector magnitudes & dot products:
+        assert(Vector(3, 4).magnitude() == 5)
+        assert(Vector(3, 0).magnitude() == 3)
+        assert(Vector(0, 4).magnitude() == 4)
+        assert(v.dot_product(v) == 9)
+        assert(Vector(1, 2).dot_product(Vector(3, 4)) == 11)
+        assert(v.dot(v) == 9)
+        assert(Vector(1, 2).dot(Vector(3, 4)) == 11)
+        assert(sqrt(v.dot_product(v)) == v.magnitude())
+        assert(sqrt(v.dot(v)) == v.magnitude())
+
+        # Angle / cosines similarity metrics:
+        assert(Vector(1, 0).cosine_similarity(Vector(1, 0)) == 1)
+        assert(Vector(1, 0).cosine_distance(Vector(1, 0)) == 0)
+        assert(Vector(1, 0).angle_between(Vector(1, 0)) == 0)
+        assert(Vector(1, 0).angle_between(Vector(1, 0), radians=True) == 0)
+        assert(Vector(1, 0).cosine_similarity(Vector(0, 1)) == 0)
+        assert(Vector(1, 0).cosine_distance(Vector(0, 1)) == 1)
+        assert(Vector(1, 0).angle_between(Vector(0, 1)) == 90)
+        assert(Vector(1, 0).angle_between(Vector(0, 1), radians=True) == pi/2)
+        assert(Vector(1, 0).cosine_similarity(Vector(-1, 0)) == -1)
+        assert(Vector(1, 0).cosine_distance(Vector(-1, 0)) == 2)
+        assert(Vector(1, 0).angle_between(Vector(-1, 0)) == 180)
+        assert(Vector(1, 0).angle_between(Vector(-1, 0), radians=True) == pi)
+        assert(
+            round(Vector(1, 0).cosine_similarity(Vector(1, 1)), PRECISION)
+            == round(cos(pi/4), PRECISION)
+        )
+        assert(
+            round(Vector(1, 0).cosine_distance(Vector(1, 1)), PRECISION)
+            == round(1 - cos(pi/4), PRECISION)
+        )
+        assert(int(Vector(1, 0).angle_between(Vector(1, 1))) == 45)
+        assert(
+            round(
+                Vector(1, 0).angle_between(Vector(1, 1), radians=True),
+                PRECISION
+            )
+            == round(pi/4, PRECISION)
+        )
 
         print("All Vector tests pass")
         return True
