25d36db777f1c5e36a1c7eaaca13924ffb346334
Sean Osier <sean@seanosier.com>
2021-03-18 00:05:10 -0400

    Implement variances, covariances, correlations

--- 03-PCA/PCA.py
+++ 03-PCA/PCA.py
@@ -13,3 +13,49 @@ def mean(*args):
         total = sum(args)
 
     return total / len(args)
+
+def variance(*args):
+    avg = mean(*args)
+
+    if len(args) == 1 and type(args[0]) == list:
+        args = args[0]
+
+    return mean([(val - avg)**2 for val in args])
+
+def standard_deviation(*args):
+    return np.sqrt(variance(*args))
+
+def covariance(*args):
+    """
+    Covariance is only meaningful for when we have >1 dimension. As such the
+    elements of args should be vectors of length > 1
+
+    Returns the covariance matrix
+    """
+    avg = mean(*args)
+
+    if len(args) == 1 and type(args[0]) == list:
+        args = args[0]
+
+    differences_vs_mean = [(val - avg) for val in args]
+    differences_vs_mean = [
+        np.expand_dims(diff, axis=1)
+        if diff.ndim == 1
+        else diff
+        for diff in differences_vs_mean
+    ]
+
+    return mean([diff @ diff.T for diff in differences_vs_mean])
+
+def correlation(*args):
+    """
+    Correlation is only meaningful for when we have >1 dimension. As such the
+    elements of args should be vectors of length > 1
+
+    Returns the correlation matrix
+    """
+    covariance_matrix = covariance(*args)
+
+    standard_devs = np.expand_dims(standard_deviation(*args), axis=1)
+
+    return covariance_matrix / (standard_devs @ standard_devs.T)
--- 03-PCA/test_PCA.py
+++ 03-PCA/test_PCA.py
@@ -5,7 +5,7 @@ To test run `pytest` in the command line
 """
 import numpy as np
 
-from PCA import mean
+from PCA import mean, variance, standard_deviation, covariance, correlation
 
 def test_mean():
     assert(mean(1, 2, 3) == 2)
@@ -25,4 +25,162 @@ def test_mean():
             np.array([3, 4, 5])
         ]),
         np.array([2, 3, 4])
+    ))
+
+def test_variance():
+    assert(variance(1, 2, 3) == 2/3)
+    assert(variance([1, 2, 3]) == 2/3)
+    assert(variance(0, 2, 4) == 8/3)
+    assert(variance(600, 470, 170, 430, 300) == 21704)
+    assert(np.array_equal(
+        variance(
+            np.array([1, 1, 0]),
+            np.array([2, 2, 2]),
+            np.array([3, 3, 4])
+        ),
+        np.array([2/3, 2/3, 8/3])
+    ))
+    assert(np.array_equal(
+        variance([
+            np.array([1, 1, 0]),
+            np.array([2, 2, 2]),
+            np.array([3, 3, 4])
+        ]),
+        np.array([2/3, 2/3, 8/3])
+    ))
+
+def test_standard_deviation():
+    assert(standard_deviation(1, 3) == 1)
+    assert(standard_deviation([1, 3]) == 1)
+    assert(standard_deviation(0, 4) == 2)
+    assert(standard_deviation(600, 470, 170, 430, 300) == np.sqrt(21704))
+    assert(np.array_equal(
+        standard_deviation(
+            np.array([1, 1, 0]),
+            np.array([3, 3, 4])
+        ),
+        np.array([1, 1, 2])
+    ))
+    assert(np.array_equal(
+        standard_deviation([
+            np.array([1, 1, 0]),
+            np.array([3, 3, 4])
+        ]),
+        np.array([1, 1, 2])
+    ))
+
+def test_covariance():
+    assert(np.array_equal(
+        covariance(
+            np.array([1, 2]),
+            np.array([5, 4])
+        ),
+        np.array([
+            [4, 2],
+            [2, 1]
+        ])
+    ))
+    assert(np.array_equal(
+        covariance([
+            np.array([1, 2]),
+            np.array([5, 4])
+        ]),
+        np.array([
+            [4, 2],
+            [2, 1]
+        ])
+    ))
+    assert(np.array_equal(
+        covariance(
+            np.array([1, 2]),
+            np.array([7, 4])
+        ),
+        np.array([
+            [9, 3],
+            [3, 1]
+        ])
+    ))
+
+def test_correlation():
+    assert(np.array_equal(
+        correlation(
+            np.array([1, 2]),
+            np.array([2, 4])
+        ),
+        np.array([
+            [1, 1],
+            [1, 1]
+        ])
+    ))
+    assert(np.array_equal(
+        correlation([
+            np.array([1, 2]),
+            np.array([2, 4])
+        ]),
+        np.array([
+            [1, 1],
+            [1, 1]
+        ])
+    ))
+    assert(np.array_equal(
+        correlation(
+            np.array([1, 2]),
+            np.array([2, -4])
+        ),
+        np.array([
+            [1, -1],
+            [-1, 1]
+        ])
+    ))
+    assert(np.array_equal(
+        correlation(
+            np.array([1, 2]),
+            np.array([1, -2]),
+            np.array([2, 2]),
+            np.array([2, -2])
+        ),
+        np.array([
+            [1, 0],
+            [0, 1]
+        ])
+    ))
+    assert(np.array_equal(
+        np.round(correlation(
+            np.array([14.2, 215]),
+            np.array([16.4, 325]),
+            np.array([11.9, 185]),
+            np.array([15.2, 332]),
+            np.array([18.5, 406]),
+            np.array([22.1, 522]),
+            np.array([19.4, 412]),
+            np.array([25.1, 614]),
+            np.array([23.4, 544]),
+            np.array([18.1, 421]),
+            np.array([22.6, 445]),
+            np.array([17.2, 408])
+        ), 4),
+        np.array([
+            [1, 0.9575],
+            [0.9575, 1]
+        ])
+    ))
+    assert(np.array_equal(
+        np.round(correlation(
+            np.array([14.2, -215]),
+            np.array([16.4, -325]),
+            np.array([11.9, -185]),
+            np.array([15.2, -332]),
+            np.array([18.5, -406]),
+            np.array([22.1, -522]),
+            np.array([19.4, -412]),
+            np.array([25.1, -614]),
+            np.array([23.4, -544]),
+            np.array([18.1, -421]),
+            np.array([22.6, -445]),
+            np.array([17.2, -408])
+        ), 4),
+        np.array([
+            [1, -0.9575],
+            [-0.9575, 1]
+        ])
     ))
